796046ad25f4cb85f8a57a4ac596e18ccc4e33e6
Prensu <136965950+Prensu@users.noreply.github.com>
2024-08-01 22:26:34 +0545

    Add files via upload

---
+++ Data_Science_Prensu/tree/main/Python_files/random_password_generator.py
@@ -0,0 +1,5 @@
+import random
+password_length = int(input("enter the length of password: "))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p =  "".join(random.sample(s,password_length ))
+print(p)
